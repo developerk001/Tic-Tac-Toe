f379e72290c3e3ae02cab0e4e3a2970dbe9f3e12
Developer_K <designersohail786@gmail.com>
2017-10-17 20:20:23 +0545

    Bugs Fixed

--- js/sketch.js
+++ js/sketch.js
@@ -2,18 +2,6 @@ const cell = document.querySelectorAll('table tr td');
 let counter = 0;
 const playerOneWin = 'Player One Win "X"';
 const playerTwoWin = 'Player Two Win "O"';
-function performAction() {
-  if (this.innerHTML === "") {
-    this.innerHTML = "X";
-    this.style.color = "#27ae60";
-  } else if (this.innerHTML === "X") {
-    this.innerHTML = "O";
-    this.style.color = "red";
-  } else if (this.innerHTML === "O") {
-    this.innerHTML = "";
-  }
-  setTimeout(win, 3000);
-}
 function win() {
   // For "O";
   if (cell[0].innerHTML === "O" && cell[1].innerHTML === "O" && cell[2].innerHTML === "O") {
@@ -51,6 +39,18 @@ function win() {
   } if (cell[2].innerHTML === "X" && cell[5].innerHTML === "X" && cell[8].innerHTML === "X") {
     alert(playerOneWin); location.reload();
   }
+}
+function performAction() {
+  if (this.innerHTML === "") {
+    this.innerHTML = "X";
+    this.style.color = "#27ae60";
+  } else if (this.innerHTML === "X") {
+    this.innerHTML = "O";
+    this.style.color = "red";
+  } else if (this.innerHTML === "O") {
+    this.innerHTML = "";
+  }
+  setTimeout(win, 3000);
 }
 for (counter = 0; counter < cell.length; counter++) {
   cell[counter].addEventListener('click', performAction);
